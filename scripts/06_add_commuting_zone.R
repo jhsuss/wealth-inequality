9a8633c67934fd2fa982d18704d8e25eed30dcf7
Joel Suss <suss.joel@gmail.com>
2023-07-04 20:17:19 +0100

    minor change

--- scripts/06_add_commuting_zone.R
+++ scripts/06_add_commuting_zone.R
@@ -1,9 +1,8 @@
 library(tidyverse)
 
-# load function which will do the computation
+# load function which will do the crosswalk
 source("functions/cz_crosswalk.R")
 
-# puma codes are different in each census file
 years <- seq(1960,2020,10)
 
 for (r in seq_along(years)) {
