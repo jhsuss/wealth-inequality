b785c320e580fe5a9e92b573afa6aac258453f2d
Joel Suss <suss.joel@gmail.com>
2024-03-01 16:37:46 +0000

    updated

--- analysis/figures_country_inequality_validation.R
+++ analysis/figures_country_inequality_validation.R
@@ -1,283 +1,117 @@
+############# country-level validation -------------
 library(tidyverse)
+library(ggrepel)
 
-wid <- read_csv("data/WID_gini.csv")
-wid_top1 <- read_csv("data/WID_top1.csv")
+load("data/wealth_inequality.Rdata")
+smith <- read_csv("data/smith_fig1b_replication_data.csv")
 
-wid <- wid_top1 %>% 
-  left_join(wid, by = c("year")) %>% 
-  rename_at(
-    vars(gini, matches("^(top|bottom)")),
-    ~ str_c("wid_",.)
-  )
-
-# load Kuhn et al data
-kuhn <- read_csv("data/kuhn_gini.csv")
-kuhn <- kuhn %>% 
-  mutate_at(
-    vars(gini_kuhn, gini90_kuhn,gini99_kuhn),
-    ~ . / 100
-  )
-# load combined ineq data
-load("wealth_inequality.Rdata")
-
-# filter for country-level and join with WID levels
-tmp <- wid %>% 
-  full_join(
-    ineq_country,
-    by = "year"
-  ) %>% 
-  distinct %>% 
-  left_join(
-    kuhn, 
-    by = "year"
-  )
-
-
-# Gini comparison
-gini <- tmp %>%
-  filter(year >= 1960) %>% 
+smith <- smith %>% 
   select(
-    year,
-    Ensemble = pred_wealth_new_gini,
-    `Saez & Zucman` = wid_gini,
-    Kuhn = gini_kuhn
-  ) %>% 
-  pivot_longer(
-    cols = c(Ensemble, Kuhn, `Saez & Zucman`),
-    names_to = "estimate"
-  ) %>% 
-  filter(!is.na(value))
-
-fig_gini <- ggplot(
-  gini,
-  aes(
-    x = year, 
-    y = value
-    )
-  ) + 
-  geom_line(
-    aes(linetype = estimate)
-    ) +
-  geom_point(
-    aes(shape = estimate)
-  ) +
-  # add CIs
-  # geom_ribbon(
-  #   data = tmp %>%
-  #     filter(
-  #       year >= 1960,
-  #       !is.na(pred_wealth_new_gini)
-  #       ),
-  #   aes(
-  #     y = pred_wealth_new_gini,
-  #     ymin = pred_wealth_new_gini_lower,
-  #     ymax = pred_wealth_new_gini_upper
-  #   ),
-  #   alpha = 0.2,
-  #   color = "gray"
-  # ) +
-  labs(x = "Year", y = "Gini", linetype = "", shape = "") +
-  theme_minimal(base_size = 14) 
-
-# share comparison
-share <- tmp %>%
-  # TODO create group for top1, top10, bottom to facet
-  #mutate()
-  select(
-    year,
-    `Ensemble Top 1%` = pred_wealth_new_top1,
-    `Ensemble Top 10%` = pred_wealth_new_top10,
-    `Ensemble Bottom 50%` = pred_wealth_new_bottom50,
-    `Saez & Zucman Top 1%` = wid_top1,
-    `Saez & Zucman Top 10%` = wid_top10,
-    `Saez & Zucman Bottom 50%` = wid_bottom50
-  ) %>% 
-  pivot_longer(
-    cols = c(`Ensemble Top 1%`:`Saez & Zucman Bottom 50%`),
-    names_to = "estimate"
-  ) %>% 
-  filter(
-    !is.na(value),
-    year >= 1960
-    )
-
-fig_share <- ggplot(
-  share,
-  aes(
-    x = year, 
-    y = value,
-    group = estimate
-  )
-  ) + 
-  geom_line(
-    aes(linetype = estimate)
-  ) +
-  geom_point(
-    aes(shape = estimate)
-  ) +
-  labs(x = "Year", y = "Proportion total wealth", linetype = "", shape = "") +
-  theme_minimal(base_size = 14)
-
-
-
-save(
-  fig_gini,
-  fig_share,
-  file = "figures/figs_country_level_validation.Rdata"
-)
-
-fig_country_validation <- cowplot::plot_grid(
-  fig_gini + 
-    theme_minimal(base_size = 14), 
-  fig_share +
-    theme_minimal(base_size = 14), 
-  nrow = 2
-  )
-
-ggsave(fig_country_validation,
-       width = 12, height = 9,
-       units = "in",
-       dpi = 300,
-       file = "figures/fig_country_validation.png")
-
-
-ggsave(fig_share,
-       width = 1900, height = 1000,
-       units = "px",
-       dpi = 300,
-       file = "figures/fig_validation_saez_shares_updated.jpg"
-       )
-
-ggsave(fig_gini,
-       width = 12, height = 9,
-       units = "in",
-       dpi = 300,
-       file = "figures/fig_country_validation_gini.jpg"
-       )
-
-
-# top .1% and .01% comparison
-saez <- read_csv("data/saez_zucman_2016.csv")
-
-# https://www.federalreserve.gov/releases/z1/dataviz/dfa/distribute/table/#quarter:129;series:Net%20worth;demographic:networth;population:all;units:shares
-dfa_shares <- read_csv("data/scf_shares.csv")
-dfa_shares <- dfa_shares %>% 
-  filter(
-    !is.na(Date),
-    str_detect(Date, "Q4$")
-  ) %>% 
-  mutate(
-    year = str_extract(Date,"^.{4}")
-  ) %>% 
-  select(
-    year, 
-    `Top 0.1% (DFA)` = `Top 0.1%`
-  ) %>% 
-  mutate_all(
-    ~ as.numeric(.)
-  )
-
-
-saez <- saez %>%
-  select(
-    year, `Top 0.1%`, `Top 10%`, `Top 0.01%`
-    ) %>%
-  mutate_at(
-    vars(`Top 0.1%`, `Top 10%`, `Top 0.01%`),
-    ~ str_replace_all(.,"%","") %>% 
-      as.numeric
+    year = Year, 
+    matches("^Top (1|0.1|0.01|)")
   ) %>% 
   full_join(
     ineq_country %>% 
       select(
         year, 
-        pred_wealth_new_top.1, 
-        pred_wealth_new_top10,
-        pred_wealth_new_top.01
-        ) %>% 
+        `Top 1% Ensemble` = top1,
+        `Top 0.1% Ensemble` = top.1,
+        `Top 0.01% Ensemble` = top.01,
+        `Top 0.001% Ensemble` = top.001
+      ) %>% 
+      filter(year >= 1950) %>% 
       mutate_at(
-        vars(pred_wealth_new_top.1, 
-             pred_wealth_new_top10,
-             pred_wealth_new_top.01),
-        ~ . * 100 
-      ),
-    by = "year"
+        vars(matches("Ensemble")),
+        ~ . * 100
+      )
   ) %>% 
-  distinct %>% 
-  full_join(
-    dfa_shares, 
-    by = "year"
+  arrange(year) %>% 
+  pivot_longer(
+    cols = -year,
+    names_to = "estimate"
+  ) %>% 
+  filter(!is.na(value)) %>% 
+  mutate(
+    threshold = str_extract(estimate, "Top.*%"),
+    estimate = str_remove(estimate, "Top.*% ")
+  ) %>% 
+  mutate(
+    estimate = ifelse(estimate == "Baseline Estimate", "SZZ (2023)", estimate),
+    estimate = ifelse(estimate == "Harmonized SCF", "SCF", estimate),
+    estimate = ifelse(estimate == "Harmonized SCF with Forbes", "SCF w/ Forbes", estimate),
+    estimate = ifelse(estimate == "Equal Return, Individual (PSZ 2018, Extended)", "PSZ (2018)", estimate),
+    estimate = ifelse(estimate == "Revised Saez Zucman (2020)", "SZ (2020)", estimate)
+  ) %>% 
+  mutate(
+    estimate = factor(
+      estimate,
+      levels = c(
+        "Ensemble", 
+        "SZZ (2023)",
+        "SZ (2020)",
+        "PSZ (2018)",
+        "SCF w/ Forbes",
+        "SCF"
+      )
+    )
   )
 
-
-fig_0.1 <- ggplot(
-  saez %>% 
-    filter(year >= 1950) %>% 
-    select(
-      year,
-      Ensemble = pred_wealth_new_top.1,
-      `Top 0.1% (Saez & Zucman)` = `Top 0.1%`,
-      `Top 0.1% (DFA)`
-    ) %>% 
-    pivot_longer(
-      cols = c(Ensemble, `Top 0.1% (Saez & Zucman)`, `Top 0.1% (DFA)`),
-      names_to = "estimate"
-    ) %>% 
-    filter(!is.na(value)),
-  aes(
-    x = year, 
-    y = value
+smith$threshold <- smith$threshold %>% 
+  factor(
+    levels = rev(
+      sort(
+        unique(smith$threshold)
+      )
+    )
   )
-) + 
-  geom_line(
-    aes(linetype = estimate)
-  ) +
-  geom_point(
-    aes(shape = estimate)
-  ) +
-  labs(x = "Year", y = "Share (%)", linetype = "", shape = "") +
-  theme_minimal(base_size = 14) 
 
-ggsave(fig_0.1,
-       width = 12, height = 9,
-       units = "in",
-       dpi = 300,
-       file = "figures/fig_validation_saez_shares_0.1.png"
-       )
-
-
-
-  
-fig_0.01 <- ggplot(
-  saez %>% 
-    filter(year >= 1950) %>% 
-    select(
-      year,
-      Ensemble = pred_wealth_new_top.01,
-      `Saez & Zucman Top 0.01%` = `Top 0.01%`
-    ) %>% 
-    pivot_longer(
-      cols = c(Ensemble, `Saez & Zucman Top 0.01%`),
-      names_to = "estimate"
-    ) %>% 
-    filter(!is.na(value)),
+country_validation <- ggplot(
+  smith %>% filter(estimate == "Ensemble", year > 1950),
   aes(
     x = year, 
     y = value
     )
   ) + 
   geom_line(
-    aes(linetype = estimate)
+    aes(group = estimate),
+    data = smith %>% filter(estimate != "Ensemble", year > 1950),
+    size = 1.1,
+    color = "gray"
   ) +
   geom_point(
-    aes(shape = estimate)
+    data = smith %>% filter(estimate != "Ensemble", year > 1950),
+    aes(group = estimate),
+    shape = 2,
+    size = 1.1,
+    color = "gray"
   ) +
-  labs(x = "Year", y = "Share (%)", linetype = "", shape = "") +
-  theme_minimal(base_size = 14) 
+  geom_line(
+    size = 1.2,
+    color = "dark orange"
+  ) +
+  geom_point(
+    size = 1.2
+  ) +
+  facet_wrap( ~ threshold, scales = "free_y", nrow = 2) +
+  labs(
+    x = "Year", y = "Share (%)" 
+  ) +
+  geom_text_repel(
+    data = smith %>% 
+      group_by(estimate) %>% 
+      filter(year == max(year)),
+    aes(label = estimate),
+    max.overlaps = 11
+  ) + 
+  theme_minimal(base_size = 20) +
+  theme(
+    legend.position = "bottom",
+    legend.key.width = unit(5,"cm"),
+    axis.text.x = element_text(angle = 90)
+  )  + 
+  scale_x_continuous(
+    n.breaks = 9
+  ) 
 
-ggsave(fig_0.01,
-       width = 12, height = 9,
-       units = "in",
-       dpi = 300,
-       file = "figures/fig_validation_saez_shares_0.01.png"
-       )
+country_validation
+
