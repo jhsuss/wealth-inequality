bb7e10068c75f2a2cf1d293dee93d855b2ab246a
Joel Suss <suss.joel@gmail.com>
2024-03-15 11:52:16 +0000

    tidy up helper functions

--- functions/inequality_measures.R
+++ functions/inequality_measures.R
@@ -1,68 +1,282 @@
-# weighted functions that can also work with boot()
-# helper functions 
+###### helper functions to compute different inequality measures 
 
-# unweighted
-# Top1 <- function(x) {
-#   sum(x[x >= quantile(x,p=0.99,na.rm=T)],na.rm = T) / sum(x,na.rm = T)
-# }
-# nine_ten <- function(x) {
-#   quantile(x,p=0.90,na.rm = T) / quantile(x,p=0.10,na.rm=T)
-# }
-# nine_five <- function(x) {
-#   quantile(x,p=0.90,na.rm=T) / quantile(x,p=0.50,na.rm=T)
-# }
-# five_ten <- function(x) {
-#   quantile(x,p=0.50,na.rm=T) / quantile(x,p=0.10,na.rm=T)
-# }
-
-
-Top.01 <- function(x,w) {
+Top.001 <- function(x,w=NULL) {
   
-  y <- sum(x[x >= Hmisc::wtd.quantile(x,probs=0.9999,na.rm=T, weights = w)],na.rm = T) 
+  if (is.null(w)) {
+    
+    y <- sum(x[x >= quantile(x,probs=0.99999,na.rm=T)],na.rm = T)
+    z <- sum(x,na.rm = T)
+    
+  } else {
+    df <- tibble(x,w)
+    
+    y <- df %>% 
+      filter(x >= Hmisc::wtd.quantile(x,probs=0.99999,na.rm=T, weights = w)) 
+    
+    # in case the top x percentile is the max obs, filter fails 
+    if (nrow(y) == 0) {
+      y <- df %>% 
+        arrange(desc(x)) %>% 
+        slice(1) %>%  
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    } else {
+      y <- y %>% 
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    }
+    
+    z <- sum(x*w,na.rm = T)
+  }
   
-  z  <-  sum(x,na.rm = T)
+  return(ifelse(z != 0, y/z, NA))
+}
+
+Top.01 <- function(x,w=NULL) {
+  
+  if (is.null(w)) {
+    
+    y <- sum(x[x >= quantile(x,probs=0.9999,na.rm=T)],na.rm = T)
+    z <- sum(x,na.rm = T)
+    
+  } else {
+    df <- tibble(x,w)
+    
+    y <- df %>% 
+      filter(x >= Hmisc::wtd.quantile(x,probs=0.9999,na.rm=T, weights = w)) 
+    
+    # in case the top x percentile is the max obs, filter fails 
+    if (nrow(y) == 0) {
+      y <- df %>% 
+        arrange(desc(x)) %>% 
+        slice(1) %>%  
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    } else {
+      y <- y %>% 
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    }
+      
+    z <- sum(x*w,na.rm = T)
+  }
   
   return(ifelse(z != 0, y/z, NA))
 }
 
 
-Top.1 <- function(x,w) {
+Top.1 <- function(x,w=NULL) {
   
-  y <- sum(x[x >= Hmisc::wtd.quantile(x,probs=0.999,na.rm=T, weights = w)],na.rm = T) 
+  if (is.null(w)) {
+    
+    y <- sum(x[x >= quantile(x,probs=0.999,na.rm=T)],na.rm = T)
+    z <- sum(x,na.rm = T)
+    
+  } else {
+    df <- tibble(x,w)
+    
+    y <- df %>% 
+      filter(x >= Hmisc::wtd.quantile(x,probs=0.999,na.rm=T, weights = w)) 
+    
+    # in case the top x percentile is the max obs, filter fails 
+    if (nrow(y) == 0) {
+      y <- df %>% 
+        arrange(desc(x)) %>% 
+        slice(1) %>%  
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    } else {
+      y <- y %>% 
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    }
+    
+    z <- sum(x*w,na.rm = T)
+  }
   
-  z  <-  sum(x,na.rm = T)
   
   return(ifelse(z != 0, y/z, NA))
 }
 
-Top1 <- function(x,w) {
+Top1 <- function(x,w = NULL) {
   
-  y <- sum(x[x >= Hmisc::wtd.quantile(x,probs=0.99,na.rm=T, weights = w)],na.rm = T) 
-  #y <- sum(x[x >= quantile(x, probs=0.99, na.rm=T)],na.rm = T) 
-  
-  z  <-  sum(x,na.rm = T)
+  if (is.null(w)) {
+    
+    y <- sum(x[x >= quantile(x,probs=0.99,na.rm=T)],na.rm = T)
+    z <- sum(x,na.rm = T)
+    
+  } else {
+    df <- tibble(x,w)
+    
+    y <- df %>% 
+      filter(x >= Hmisc::wtd.quantile(x,probs=0.99,na.rm=T, weights = w)) 
+    
+    # in case the top x percentile is the max obs, filter fails 
+    if (nrow(y) == 0) {
+      y <- df %>% 
+        arrange(desc(x)) %>% 
+        slice(1) %>%  
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    } else {
+      y <- y %>% 
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    }
+    
+    z <- sum(x*w,na.rm = T)
+  }
   
   return(ifelse(z != 0, y/z, NA))
 }
 
-Top10 <- function(x,w) {
+Top10 <- function(x,w=NULL) {
   
-  y <- sum(x[x >= Hmisc::wtd.quantile(x,probs=0.90,na.rm=T, weights = w)],na.rm = T) 
+  if (is.null(w)) {
+    
+    y <- sum(x[x >= quantile(x,probs=0.90,na.rm=T)],na.rm = T)
+    z <- sum(x,na.rm = T)
+    
+  } else {
+    df <- tibble(x,w)
+    
+    y <- df %>% 
+      filter(x >= Hmisc::wtd.quantile(x,probs=0.90,na.rm=T, weights = w)) 
+    
+    # in case the top x percentile is the max obs, filter fails 
+    if (nrow(y) == 0) {
+      y <- df %>% 
+        arrange(desc(x)) %>% 
+        slice(1) %>%  
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    } else {
+      y <- y %>% 
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    }
+    
+    z <- sum(x*w,na.rm = T)
+  }
+  return(ifelse(z != 0, y/z, NA))
+}
+
+Bottom50 <- function(x,w=NULL) {
   
-  z  <-  sum(x,na.rm = T)
+  if (is.null(w)) {
+    
+    y <- sum(x[x <= quantile(x,probs=0.50,na.rm=T)],na.rm = T)
+    z <- sum(x,na.rm = T)
+    
+  } else {
+    df <- tibble(x,w)
+    
+    y <- df %>% 
+      filter(x <= Hmisc::wtd.quantile(x,probs=0.50,na.rm=T, weights = w)) %>% 
+      summarise(
+        x = sum(x*w)
+      ) %>% 
+      .$x
+    
+    z <- sum(x*w,na.rm = T)
+  }
   
   return(ifelse(z != 0, y/z, NA))
 }
 
-Bottom50 <- function(x,w) {
+
+Top.5 <- function(x,w=NULL) {
   
-  y <- sum(x[x <= Hmisc::wtd.quantile(x,probs=0.50,na.rm=T, weights = w)],na.rm = T) 
+  if (is.null(w)) {
+    
+    y <- sum(x[x >= quantile(x,probs=0.995,na.rm=T)],na.rm = T)
+    z <- sum(x,na.rm = T)
+    
+  } else {
+    df <- tibble(x,w)
+    
+    y <- df %>% 
+      filter(x >= Hmisc::wtd.quantile(x,probs=0.995,na.rm=T, weights = w)) 
+    
+    # in case the top x percentile is the max obs, filter fails 
+    if (nrow(y) == 0) {
+      y <- df %>% 
+        arrange(desc(x)) %>% 
+        slice(1) %>%  
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    } else {
+      y <- y %>% 
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    }
+    
+    z <- sum(x*w,na.rm = T)
+  }
   
-  z  <-  sum(x,na.rm = T)
   
   return(ifelse(z != 0, y/z, NA))
 }
 
+Top5 <- function(x,w = NULL) {
+  
+  if (is.null(w)) {
+    
+    y <- sum(x[x >= quantile(x,probs=0.95,na.rm=T)],na.rm = T)
+    z <- sum(x,na.rm = T)
+    
+  } else {
+    df <- tibble(x,w)
+    
+    y <- df %>% 
+      filter(x >= Hmisc::wtd.quantile(x,probs=0.95,na.rm=T, weights = w)) 
+    
+    # in case the top x percentile is the max obs, filter fails 
+    if (nrow(y) == 0) {
+      y <- df %>% 
+        arrange(desc(x)) %>% 
+        slice(1) %>%  
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    } else {
+      y <- y %>% 
+        summarise(
+          x = sum(x*w)
+        ) %>% 
+        .$x
+    }
+    
+    z <- sum(x*w,na.rm = T)
+  }
+  
+  return(ifelse(z != 0, y/z, NA))
+}
 
 nine_ten <- function(x,w) {
   
@@ -88,43 +302,15 @@ nine_five <- function(x,w) {
   return(ifelse(z != 0, y/z, NA))
 }
 
-nine_five.fun <- function(d, i, j) {
-  d <- d[i, ]
-  w <- j[i, ]
-  
-  y <- Hmisc::wtd.quantile(d[["pred_wealth"]],probs=0.90,na.rm = T,weights=w[[wght]] %>% as.numeric)
-  z <- Hmisc::wtd.quantile(d[["pred_wealth"]],probs=0.50,na.rm = T,weights=w[[wght]] %>% as.numeric)
-  return(ifelse(z != 0, y/z, NA))
-  
-}
 
+#### functions to work with boot()
 
 mean.fun <- function(dat, idx) mean(dat[idx], na.rm = TRUE)
 
 sd.fun <- function(dat, idx) sd(dat[idx], na.rm = TRUE)
 
 
-gini.fun <- function(d, i, j) {
-  d <- d[i, ]
-  w <- j[i, ]
-  
-  return(
-    gini(d[["pred_wealth"]], 
-              w[[wght]] %>% as.numeric)
-    )   
-}
-
-gini_new.fun <- function(d, i, j) {
-  d <- d[i, ]
-  w <- j[i, ]
-  
-  return(
-    gini(d[["pred_wealth_new"]], 
-         w[[wght]] %>% as.numeric)
-  )   
-}
-
-gini_new.fun <- function(d, i, j, var) {
+gini.fun <- function(d, i, j, var) {
   d <- d[i, ]
   w <- j[i, ]
   
@@ -133,29 +319,6 @@ gini_new.fun <- function(d, i, j, var) {
          w[[wght]] %>% as.numeric)
   )   
 }
-
-
-gini_ihs.fun <- function(d, i, j) {
-  d <- d[i, ]
-  w <- j[i, ]
-  
-  return(
-    gini(d[["pred_wealth_ihs"]], 
-         w[[wght]] %>% as.numeric)
-  )   
-}
-
-gini_net.fun <- function(d, i, j) {
-  d <- d[i, ]
-  w <- j[i, ]
-  
-  return(
-    gini(d[["pred_wealth_net"]], 
-         w[[wght]] %>% as.numeric)
-  )   
-}
-
-
 
 boot.fun <- function(d, i, j, var, fct = gini) {
   d <- d[i, ]
